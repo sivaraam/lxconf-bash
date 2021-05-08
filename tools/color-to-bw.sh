437aa0ab24ed97c9e0de74c52ff09287e0762a36
Kaartic Sivaraam <kaartic.sivaraam@gmail.com>
2021-05-09 00:06:03 +0530

    Add script to convert color PDF to black&white

---
+++ tools/color-to-bw.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Script to convert a color PDF file to black-and-white
+# using GhostScript.
+
+gs \
+ -sOutputFile=output.pdf \
+ -sDEVICE=pdfwrite \
+ -sColorConversionStrategy=Gray \
+ -dProcessColorModel=/DeviceGray \
+ -dCompatibilityLevel=1.4 \
+ -dNOPAUSE \
+ -dBATCH \
+ $1
+
